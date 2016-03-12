0db843b005cbe479618010b4f1e2bf80691b01ea
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-03-12 19:06:48 +0100

    Improves the fullscreen behaviour

--- src/android/com/mesmotronic/plugins/FullScreenPlugin.java
+++ src/android/com/mesmotronic/plugins/FullScreenPlugin.java
@@ -172,7 +172,10 @@ public class FullScreenPlugin extends CordovaPlugin
 					
 					int uiOptions = 
 						View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_FULLSCREEN;
+						| View.SYSTEM_UI_FLAG_FULLSCREEN
+			            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+			            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 					
 					decorView.setOnSystemUiVisibilityChangeListener(null);
 					decorView.setSystemUiVisibility(uiOptions);
